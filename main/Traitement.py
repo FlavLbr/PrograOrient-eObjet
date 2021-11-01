6fa3ca265c202ddf977a710ee7e86af154371867
FlavLbr <79084721+FlavLbr@users.noreply.github.com>
2021-11-01 16:12:55 +0100

    Collecte fait
    Thomas a fait le package collecte non commenté
    
    Co-Authored-By: thgerault <91197552+thgerault@users.noreply.github.com>

--- main/Traitement.py
+++ main/Traitement.py
@@ -1,15 +1,22 @@
+from collecte import Collecte
+
 class Traitement:
     def __init__(self):
-        ...
+        self.text1=text1
+        self.text2=text2
 
     def load(self, fichier):
     #génère plusieurs page ou PDF
-        ...
+        une_collecte=Collecte.run()
+        return Collecte.content
     
     def run(self):
         #génère le résultat
-        ...
+        b="/n"
+        self.text1=Collecte.content[0].replace(b,"")
+        self.text2=Collecte.content[1].replace(b,"")
+        self.texts=[self.text1,self.text2]
     
     def show(self):
         #affiche le résultat
-        ...
+        return self.text
