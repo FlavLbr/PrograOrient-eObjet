8694c69faa847ca439d011d1abfd58badba4c7a7
Flavien <flebreto@etud.univ-angers.fr>
2021-11-04 17:18:18 +0100

    changement de lien

--- main/main.py
+++ main/main.py
@@ -14,14 +14,14 @@ print(r)"""
 une_collecte.run()                
 print(une_collecte.content())"""
 
-"""
+
 le_traitement = Traitement()
-le_traitement.load(["/users/2022ds/118005444/Bureau/pdf1.pdf", "https://math.univ-angers.fr/~jaclin/_site2022_poO19_ds1/python_objet/2022/heritage/heritage.html"])
+le_traitement.load(["/run/media/flebreto/94D2-C54D/pdf1.pdf", "https://math.univ-angers.fr/~jaclin/_site2022_poO19_ds1/python_objet/2022/heritage/heritage.html"])
 le_traitement.run()         
-le_traitement.show()"""
+le_traitement.show()
 
 
-
+"""
 un_prisme = Prisme('Nuage')      # crée le prisme générateur d'un nuage de mots
-un_prisme.run("/users/2022ds/118005444/Bureau/liens.txt") 
-un_prisme.show()
+un_prisme.run("/run/media/flebreto/94D2-C54D/liens.txt") 
+un_prisme.show()"""
